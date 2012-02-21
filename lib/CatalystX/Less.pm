03b1f9ab9a1ad285d1c2bb986b67d1995cb13308
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-02-21 12:57:34 +0100

    Initial commit

---
+++ lib/CatalystX/Less.pm
@@ -0,0 +1,26 @@
+package CatalystX::Less;
+use Moose::Role;
+use namespace::autoclean;
+
+1;
+
+=head1 NAME
+
+CatalystX::Less - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 the above author(s).
+
+This sofware is free software, and is licensed under the same terms as perl itself.
+
+=cut
+
