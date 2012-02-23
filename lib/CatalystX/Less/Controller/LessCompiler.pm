ac591e092fc860494af1226285bca8cb2447c507
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-02-23 14:03:19 +0100

    Re-order some code to make it clearer

--- lib/CatalystX/Less/Controller/LessCompiler.pm
+++ lib/CatalystX/Less/Controller/LessCompiler.pm
@@ -20,12 +20,13 @@ sub less : Path('') Args(1) {
     my @files = map { $_ . '.less' } split(";", $files);
     # compile and return
     my $cfg = $c->config->{'CatalystX::Less'};
+
+    $c->res->content_type('text/css');
     my $base_folder = $cfg->{base_folder} || 'root/static/less/';
 
     # If the config is absolute path, leave it alone
     $base_folder = $c->path_to($base_folder) unless $base_folder =~ m|^/|;
 
-    $c->res->content_type('text/css');
     foreach my $file (@files) {
         my $full = Path::Class::File->new($base_folder, $file);
         unless (-f $full) {
