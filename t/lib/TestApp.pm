7639d24b9ce3a54b874ad0014785b705d6342784
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-02-23 14:04:31 +0100

    Make test app load config from env if it is there

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -1,11 +1,11 @@
 package TestApp;
 use Moose;
 use namespace::autoclean;
-
+our $VERSION = '0.01';
 use Catalyst qw/+CatalystX::Less/;
 
 extends 'Catalyst';
-__PACKAGE__->config();
+__PACKAGE__->config(eval ($ENV{TESTAPP_CONFIG} || '{}'));
 __PACKAGE__->setup;
 
 1;
