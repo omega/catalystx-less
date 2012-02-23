089ce95d788c734b76b923700e30471e402fe5cd
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-02-23 08:37:22 +0100

    Document main extension class

--- lib/CatalystX/Less.pm
+++ lib/CatalystX/Less.pm
@@ -1,4 +1,26 @@
 package CatalystX::Less;
+#ABSTRACT: Provide easy access to less as css
+
+=head1 SYNOPSIS
+
+    # in YourApp.pm
+    use Catalyst qw/+CatalystX::Less/;
+
+    # in root/wrapper.tt
+    <link rel="stylesheet" href="[% c.uri_for_combined_less('base.less', 'fonts.less') %]"></link>
+
+=head1 JUSTIFICATION
+
+Working with LESS is much prefered by most front end developers, but getting
+the toolchain just right for everyone can be a chore. What we do here, is we
+set up a less compiler endpoint in the app. This end point compiles the less
+into css on request. In our production environment we just cache this with
+varnish, so the added time isn't an issue.
+
+=cut
+
+
+
 use Moose::Role;
 use namespace::autoclean;
 use CatalystX::InjectComponent;
@@ -11,6 +33,15 @@ after 'setup_components' => sub {
         as => 'Controller::Less',
     );
 };
+
+=method uri_for_combined_less(@less_files)
+
+This will return a special url to the (auto-magical) LessCompiler controllers
+less action. The url will include parts of the filenames.
+
+
+=cut
+
 sub uri_for_combined_less {
     my $c = shift;
     my $cfg = $c->config->{'CatalystX::Less'};
@@ -20,29 +51,12 @@ sub uri_for_combined_less {
 
     # We strip out .less and join by ; for now
     my $encoded = join(";", (map { s/\.less$//; $_;} @lesses));
-    my $uri = $c->uri_for('/less',  $encoded  . ".css");
+    my $uri = $c->uri_for($c->controller('Less')->action_for('less'),  $encoded  . ".css");
     return $uri;
 }
 
 1;
 
-=head1 NAME
 
-CatalystX::Less - 
 
-=head1 DESCRIPTION
-
-=head1 METHODS
-
-=head1 BUGS
-
-=head1 AUTHOR
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 the above author(s).
-
-This sofware is free software, and is licensed under the same terms as perl itself.
-
-=cut
 
